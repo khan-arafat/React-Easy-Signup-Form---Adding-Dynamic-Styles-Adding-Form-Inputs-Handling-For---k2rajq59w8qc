cc691159cf8524d8c827c29d5a62b5bbadfd65a9
Arafat <physics.propagation@gmail.com>
2023-02-24 20:14:08 +0530

    edited app component

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,19 @@ import { signUpFormValidation } from '../utils/validation.js';
 const App = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [passcode, setPasscode] = useState('');
+  const [password, setPassword] = useState('');
   const [nameDisplay, setNameDisplay] = useState('');
   const [emailDisplay, setEmailDisplay] = useState('');
   const [passDisplay, setPassDisplay] = useState('');
-  const validateForm = ()=>{
+  function validateForm(){
     const dataCollected = {
       name: `${name}`,
       email:`${email}`,
-      password: `${passcode}`
+      password: `${password}`,
     };
     let errorGenerated = signUpFormValidation(dataCollected);
       Object.keys(errorGenerated).map((er)=>{
+        console.log(errorGenerated);
         if(er==='name'){
           setNameDisplay(errorGenerated.name);
         }
@@ -38,24 +39,30 @@ const App = () => {
         }
     })
   }
+  const verified = (event)=>{
+    event.preventDefault();
+    if(nameDisplay!=='' || passDisplay!=='' || emailDisplay!==''){
+      return;
+    }
+  }
   return (
     <>
-    <form onSubmit={(e)=>e.preventDefault()}>
+    <form onSubmit={verified}>
       <div>
       <label htmlFor='name'>Name:</label>
-      <input type='text' id='name' onChange={(e)=>{setName(e.target.value); validateForm();}}></input>
+      <input type='text' id='name' onChange={(e)=>{setName(e.target.value); validateForm()}}></input>
       </div>
-      {nameDisplay}
+      <div>{nameDisplay}</div>
       <div>
       <label htmlFor='email'>Email:</label>
-      <input type='text' id='email' onChange={(e)=>{setEmail(e.target.value); validateForm();}}></input>
+      <input type='text' id='email' onChange={(e)=>{setEmail(e.target.value); validateForm()}}></input>
       </div>
-      {emailDisplay}
+      <div>{emailDisplay}</div>
       <div>
       <label htmlFor='password'>Password:</label>
-      <input type='password' id='password' onChange={(e)=>{setPasscode(e.target.value); validateForm();}}></input>
+      <input type='text' id='password' onChange={(e)=>{setPassword(e.target.value); validateForm()}}></input>
       </div>
-      {passDisplay}
+      <div>{passDisplay}</div>
       <div>
       <label htmlFor='consent'>I agree?</label>
       <input type='checkbox' id='consent'></input>
